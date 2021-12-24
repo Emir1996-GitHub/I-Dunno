13b52e9d6b21f20d510aaf68d0a464247ae7fed0
AhmetCakir1 <>
2021-12-24 14:50:19 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading;
+using System.Media;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Program
+{
+ class Program
+ {
+	 static void Main(String[] args)
+	 {
+    Console.WriteLine("MS-DOS: 1.0");
+		Console.WriteLine("Hard Disk Fatal Error: Hard Disk Not Found");
+		Thread.Sleep(813523186);
+	 }
+ }
+}
